15580de8ac3dd210471c7c75f8c7b60caa2bdd66
poorbaby <gongyunrou@gmail.com>
2017-10-18 21:48:59 -0400

    change function name

--- R/paircorrelation.R
+++ R/paircorrelation.R
@@ -1,5 +1,5 @@
-#' Pair Correlation Calculation
-#' @description To calculate all the correlation between two continues variable in a given dataset.
+#' Correlation Matrix Visualization
+#' @description To calculate and visualize all the correlation between two continues variable in a given dataset.
 #'
 #' @param data a dataframe
 #' @param digits the correlation value round to the given digits, by default the digits equal to 2
@@ -9,7 +9,7 @@
 #' @import corrplot
 #' @examples
 #' paircorr(sleepduration)
-paircorr <- function(data, digits = 2) {
+cor_plot <- function(data, digits = 2) {
   con_var <- character()
   for (i in colnames(data)) {
     if(class(data[[i]]) %in% c("numeric","integer")) {
