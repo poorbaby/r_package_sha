e98a0427b1722434ac513367668d10994a7b0c6a
poorbaby <gongyunrou@gmail.com>
2017-10-18 21:33:05 -0400

    visulize the function

--- R/paircorrelation.R
+++ R/paircorrelation.R
@@ -3,9 +3,10 @@
 #'
 #' @param data a dataframe
 #' @param digits the correlation value round to the given digits, by default the digits equal to 2
-#' @return a matrix
+#' @return a plot
 #' @export
 #' @importFrom stats cor
+#' @import corrplot
 #' @examples
 #' paircorr(sleepduration)
 paircorr <- function(data, digits = 2) {
@@ -15,7 +16,7 @@ paircorr <- function(data, digits = 2) {
       con_var <- c(con_var,i)
     }
   }
-  r <- cor(data[,con_var])
-  return(round(r, digits = digits))
+  r <- round(cor(data[,con_var]), digits = digits)
+  return (corrplot::corrplot(r, method = "number",tl.srt = 45))
 }
 
